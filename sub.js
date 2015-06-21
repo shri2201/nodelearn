fb0c3659250b2584cf377f6ea35b96f7378dd2c9
Anurag Shrivastava <anurag@kennissoftware.com>
2015-06-21 12:14:55 +0200

    Added extra messages in the file

--- sub.js
+++ sub.js
@@ -1,15 +1,11 @@
+//Listen to tcp port for the messages 
 const fs = require('fs');
 const zmq = require('zmq');
-
 subscriber = zmq.socket('sub');
-
 subscriber.subscribe("");
-
 subscriber.on("message", function(data){
-	let
-	message = JSON.parse(data),
+	message = JSON.parse(data);
 	date = new Date(message.timestamp);
-    console.log("Got a change at " + date);
+    console.log("Got a change at: " + date + " File Name: " + message.file);
 });
-
 subscriber.connect("tcp://localhost:5432");
